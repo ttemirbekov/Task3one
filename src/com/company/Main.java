48dd448bf91d8a02486da1c072d560c928f7bf97
turatbek <temirbekov.tt@gmail.com>
2022-04-09 11:36:33 +0600

    Task #3.1

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        byte a = scan.nextByte();
+        byte b = scan.nextByte();
+        byte c = scan.nextByte();
+
+        System.out.println(a * 3600 + b * 601  + c);
+
+
+    }
+}
